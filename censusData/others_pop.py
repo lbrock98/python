46862bf083bbe8d6564520c09688fc489b68a1f9
lbrock98 <85496132+lbrock98@users.noreply.github.com>
2021-08-15 17:24:20 -0400

    Update others_pop.py

--- censusData/others_pop.py
+++ censusData/others_pop.py
@@ -8,7 +8,7 @@ import pandas as pd
 year = 2019
 
 # get population of US
-portfolio = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=us:*&key=fe6d5b03221189e31343c50fb863de67db0d30e9')
+portfolio = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=us:*&key={CENSUS_KEY}')
 p_data = portfolio.read().decode('utf-8')
 p_data = p_data[1:-1]
 header = p_data.split('],')[0] + ']'
@@ -20,7 +20,7 @@ p_pop['geo_type'] = 'portfolio'
 p_pop['metric'] = 'population'
 
 # get population by state 
-state = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=state:*&key=fe6d5b03221189e31343c50fb863de67db0d30e9')
+state = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=state:*&key={CENSUS_KEY}')
 s_data = state.read().decode('utf-8')
 s_data = s_data[1:-1]
 header = s_data.split('],')[0] + ']'
@@ -32,7 +32,7 @@ s_pop['geo_type'] = 'state'
 s_pop['metric'] = 'population'
 
 # get population by MSA 
-msa = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&key=fe6d5b03221189e31343c50fb863de67db0d30e9')
+msa = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=metropolitan%20statistical%20area/micropolitan%20statistical%20area:*&key={CENSUS_KEY}')
 m_data = msa.read().decode('utf-8')
 m_data = m_data[1:-1]
 header = m_data.split('],')[0] + ']'
@@ -44,7 +44,7 @@ for i in range(len(m_pop)):
     if 'PR' in m_pop['name'][i]:
         m_pop = m_pop.drop([i])
 m_pop = m_pop.reset_index(drop = True)
-state_fips = pd.read_csv("/Users/Lucy/Documents/Work/Ichor/censusData/state_fips.csv")
+state_fips = pd.read_csv("/Users/LBrock/Documents/censusData/state_fips.csv")
 m_pop['state'] = m_pop['geo_id']
 for i in range(len(m_pop)): 
     x = m_pop['name'][i].split(', ')[1][:2]
@@ -59,7 +59,7 @@ m_pop['geo_type'] = 'msa'
 m_pop['metric'] = 'population'
 
 # get population by county 
-county = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=county:*&in=state:*&key=fe6d5b03221189e31343c50fb863de67db0d30e9')
+county = urllib.request.urlopen(f'https://api.census.gov/data/{year}/pep/population?get=NAME,POP&for=county:*&in=state:*&key={CENSUS_KEY}')
 c_data = county.read().decode('utf-8')
 c_data = c_data[1:-1]
 header = c_data.split('],')[0] + ']'
@@ -81,16 +81,16 @@ cols = [cols[i] for i in reorder]
 final = final[cols]
 
 #export to csv 
-final.to_csv('/Users/Lucy/Documents/Work/Ichor/censusData/population/others_pop.csv', header=False, index=False)
+final.to_csv('/Users/LBrock/Documents/censusData/population/others_pop.csv', header=False, index=False)
 
 #export to pgadmin
-with open("/Users/Lucy/Documents/Work/Ichor/censusData/pgAdmin_info.json") as f:
+with open("/Users/LBrock/Documents/censusData/pgAdmin_info.json") as f:
     conf=json.load(f)
 
 conn = psycopg2.connect("dbname="+conf['dbname']+" user="+conf['user']+" password="+conf['password']+" host="+conf['host']+" port="+conf['port'])  
 cur = conn.cursor(cursor_factory=RealDictCursor)
 
-file = open('/Users/Lucy/Documents/Work/Ichor/censusData/population/others_pop.csv')
+file = open('/Users/LBrock/Documents/censusData/population/others_pop.csv')
 
 cur.copy_from(file,'population.census_data', sep=',', columns = ('year', 'geo_type', 'geo_id', 'metric', 'metric_value'), null = '') 
 
